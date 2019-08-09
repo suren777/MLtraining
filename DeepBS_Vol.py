cb9b8fb0003688045e3ce672a6753107688105f3
si777 <suren.islyaev@gmail.com>
2019-08-09 13:42:10 +0100

    Update

--- DeepBS_Vol.py
+++ DeepBS_Vol.py
@@ -14,7 +14,7 @@ import pandas as pd
 from scipy.stats import norm
 import seaborn as sns
 sns.set_context("notebook", font_scale=0.9, rc={"lines.linewidth": 2.5})
-sns.set_style('white')
+sns.set_style('dark')
 
 #%%
 def blackScholesPriceTrue(S0, r, K, T, vol):
@@ -28,21 +28,22 @@ def blackScholesPriceTrue(S0, r, K, T, vol):
     return n1 - Strike/F * n2
 
 def createDataset(size):
-    S0 = np.linspace(50,150, size)
-    r = np.linspace(0.0,0.02, size)
-    K = np.linspace(0.5,1.5, size)
-    T = np.linspace(0.1,1,size)
-    vol = np.linspace(0.01, 0.5, size)
-    inputs = np.array(np.meshgrid(S0, r, K, T, vol)).T.reshape(-1, 5)
-    options = blackScholesPriceTrue(inputs[:,0],inputs[:,1], inputs[:,2], inputs[:,3],inputs[:,4])
+    S0 = np.random.uniform(50,150, size)
+    r = np.random.uniform(0.001,0.03, size)
+    K = np.random.uniform(0.5,1.5, size)
+    T = np.random.uniform(0.5,1,size)
+    vol = np.random.uniform(0.01, 0.5, size)
+    options = blackScholesPriceTrue(S0, r, K, T, vol)
     columns = "spots,rates,strikes,tenors,optionPrices,vols".split(',')
-    data = np.array([inputs[:, 0],inputs[:, 1], inputs[:, 2], inputs[:, 3], options, inputs[:,4]])
+    data = np.array([S0, r, K, T, options, vol])
     data = pd.DataFrame(data=data.T, columns=columns)
+    data = data[data["optionPrices"]>=0.01]
     return data
 
 #%%
 #data = pd.read_csv("bsFrame.csv")
-data = createDataset(10)
+sample_size = 100000
+data = createDataset(sample_size)
 data.tail()
 
 #%% Load Data and normalise
@@ -55,21 +56,24 @@ train_stats = train_stats.transpose()
 train_labels = train_data.pop('vols')
 test_labels = test_data.pop('vols')
 
-def norm(x):
+def normalize(x):
     return (x - train_stats['mean']) / train_stats['std']
 #%%
-normed_train_data = norm(train_data)
-normed_test_data = norm(test_data)
+normed_train_data = normalize(train_data)
+normed_test_data = normalize(test_data)
 
 #%% Cool plot
 #sns.pairplot(train_data.loc[:500, :],diag_kind="kde")
 #%% Define model
-model=keras.Sequential( [layers.Dense(64, activation='relu', input_shape=[len(train_data.keys())]),
-layers.Dense(64, activation="relu"),
-layers.Dense(64, activation="relu"),
-layers.Dense(64, activation="relu"),
-layers.Dense(1)])
-optimizer = tf.keras.optimizers.RMSprop(0.001)
+model=keras.Sequential( [
+    layers.Dense(128, activation='relu', input_shape=[len(train_data.keys())]),
+    layers.Dense(128, activation="tanh"),
+    layers.Dense(128, activation="tanh"),
+    layers.Dense(128, activation="tanh"),
+    layers.Dense(128, activation="tanh"),
+    layers.Dense(1, activation="elu"),
+    layers.Dense(1)])
+optimizer = tf.keras.optimizers.RMSprop(0.01)
 model.compile(loss='mse',
             optimizer=optimizer,
             metrics=['mae', 'mse'])
@@ -77,19 +81,19 @@ model.summary()
 #%%
 
 modelFile = 'bsCalibratorModel.hdf5'
-epochs = 10
+epochs = 1000
 
 checkpoint = callbacks.ModelCheckpoint(modelFile, monitor='val_loss')
 reduce_lr = callbacks.ReduceLROnPlateau(monitor='val_loss', factor=0.2, patience=5, min_lr=0.00001)
-early_stop = keras.callbacks.EarlyStopping(monitor='val_loss', patience=10)
+early_stop = keras.callbacks.EarlyStopping(monitor='val_loss', patience=50)
 
 class PrintDot(keras.callbacks.Callback):
     def on_epoch_end(self, epoch, logs):
         if epoch % 100 == 0: print('')
         print('.', end='')
     
-history = model.fit(normed_train_data, train_labels,
-    epochs=epochs, validation_split = 0.2, verbose=0,
+history = model.fit(normed_train_data, train_labels,  batch_size=1024,
+    epochs=epochs, validation_split = 0.2, verbose=0, shuffle=True,
     callbacks=[PrintDot(), checkpoint, reduce_lr, early_stop])
 
 #%%
@@ -100,7 +104,7 @@ def plot_history(history):
   
     plt.figure()
     plt.xlabel('Epoch')
-    plt.ylabel('Mean Abs Error [MPG]')
+    plt.ylabel('Mean Abs Error [Vols]')
     plt.plot(hist['epoch'], hist['mae'],
              label='Train Error')
     plt.plot(hist['epoch'], hist['val_mae'],
@@ -130,8 +134,8 @@ plt.xlabel('True Values vol')
 plt.ylabel('Predictions vol')
 plt.axis('equal')
 plt.axis('square')
-plt.xlim([0.3,plt.xlim()[1]])
-plt.ylim([0.3,plt.ylim()[1]])
+# plt.xlim([0.0, plt.xlim()[1]])
+# plt.ylim([0.0, plt.ylim()[1]])
 _ = plt.plot([-100, 100], [-100, 100])
 
 #%%
@@ -139,3 +143,5 @@ error = test_predictions - test_labels
 plt.hist(error, bins = 25)
 plt.xlabel("Prediction Error [vols]")
 _ = plt.ylabel("Count")
+
+#%%
